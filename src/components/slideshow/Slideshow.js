0fd0396bc8bc30a39efacdb6876157e698760cfd
Angie pons <a.lartillier@gmail.com>
2023-01-18 16:38:15 +0100

    Create handleClick function for slideshow

--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -5,25 +5,35 @@ import ARROW_FOREWARD from '../../assets/images/arrow_forward.png'
 
 const Slideshow = ({ pictures, alt }) => {
 
-    const [index, setIndex] = useState(0)
-    const pictureNbr = pictures.length;
-
+    const [currentIndex, setIndex] = useState(0)
+    const lastIndex = pictures.length - 1;
 
     const handleClickSlideLeft = () => {
-        // quand je clic sur la fleche gauche change l'image (index-1)
+        // Si je suis a index 0 , alors isFirstSlide = vrai , je suis sur la première slide
+        const isFirstSlide = currentIndex === 0;
+        // Si Je suis à la premiere slide, alors je vais à la dernière image de mon tableau , sinon je recule de 1
+        const newIndex = isFirstSlide ? lastIndex : currentIndex - 1;
+        setIndex(newIndex)
+
     }
     const handleClickSlideRight = () => {
-        // quand je clic sur la fleche droite change l'image (index+1)
+
+        const isLastSlide = currentIndex === lastIndex;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setIndex(newIndex);
+
     }
 
     return (
-        <div className='Slideshow' style={{ backgroundImage: `url("${pictures[index]}")` }}>
+        <div className='Slideshow' style={{ backgroundImage: `url("${pictures[currentIndex]}")` }}>
 
-            {/* <img src={pictures[index]} alt={alt}></img> */}
-            <div className='Slideshow__arrows'> {pictureNbr > 1 && <img className='arrow_back' src={ARROW_BACK}></img>}
-                {pictureNbr > 1 && <img className='arrow_forward' src={ARROW_FOREWARD}></img>}</div>
 
-            {<span>{index + 1}/{pictureNbr}</span>}
+            <div className='Slideshow__arrows'>
+                {pictures.length > 1 && <img className='arrow_back' src={ARROW_BACK} alt="flèche de défilement" onClick={handleClickSlideLeft}></img>}
+                {pictures.length > 1 && <img className='arrow_forward' src={ARROW_FOREWARD} alt="flèche de défilement" onClick={handleClickSlideRight}></img>}
+            </div>
+
+            {<span>{currentIndex + 1}/{pictures.length}</span>}
 
 
         </div>
